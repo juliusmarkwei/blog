ac709283f5ffae3707668097990dff1c5b1d32af
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-09 06:43:26 +0000

    adding generic views from rest framework

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    pass
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
