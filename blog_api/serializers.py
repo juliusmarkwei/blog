b3d1c4e1de212517ea91c4a706bd7f513ca1ca95
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-15 10:47:19 +0000

    Delete blog_api directory

--- blog_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from blog.models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ("id", "title", "author", "excerpt", "content", "status")
--- blog_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PostViewSet
-from rest_framework.routers import DefaultRouter
-
-app_name = "blog_api"
-
-routers = DefaultRouter()
-routers.register("", PostViewSet, basename="post")
-urlpatterns = routers.urls
