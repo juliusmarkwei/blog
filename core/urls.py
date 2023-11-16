ec33686b1564a726027339194000ba69ebd40052
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-16 17:49:36 +0000

    added schema endpoints

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -28,5 +30,13 @@ urlpatterns = [
     path("api/", include("blog_api.urls", namespace="blog_api")),
     # path('api/user/', include('users.urls', namespace='users')),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    # path("", include("blog.urls", namespace="blog")),
+    path("", include("blog.urls", namespace="blog")),
+    path(
+        "schema",
+        get_schema_view(
+            title="BlogAPI", description="API for the BlogAPI", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
+    path('docs/', include_docs_urls(title='BlogAPI')),
 ]
