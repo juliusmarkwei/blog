83b355d734a354967fc5cd843d717c2878edc924
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-16 17:50:30 +0000

    testing api view

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,18 +1,25 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
+from rest_framework.permissions import (
+    SAFE_METHODS,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+    BasePermission,
+    IsAdminUser,
+    DjangoModelPermissions,
+)
 from rest_framework import viewsets
 from rest_framework import filters
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 
 class PostUserWritePermission(BasePermission):
-    message = 'Editing posts is restricted to the author only.'
+    message = "Editing posts is restricted to the author only."
 
     def has_object_permission(self, request, view, obj):
-
         if request.method in SAFE_METHODS:
             return True
 
@@ -24,7 +31,8 @@ class PostList(viewsets.ModelViewSet, PostUserWritePermission):
     serializer_class = PostSerializer
 
     def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get('pk')
+        item = self.kwargs.get("pk")
+
         return get_object_or_404(Post, slug=item)
 
     # Define Custom Queryset
