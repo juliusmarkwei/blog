12e4a17d63fef1d31106c41b8cbe634fc5339f7a
Julius Markwei <julius.markwei@stu.ucc.edu.gh>
2023-11-16 17:51:07 +0000

    added Templates view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_data"] = Post.objects.all()
+        return context
