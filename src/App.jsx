7513a0e0ce9b398cd28b69719ebd6ec647eaa604
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-11 04:26:27 +0000

    app component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+      <>
+        <Header />
+        <Content />
+        <Footer />
+      </>
+    );
+}
+
+export default App;
