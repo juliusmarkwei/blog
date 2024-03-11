44656599a78b439bfe9622dfdb851f99d434a01a
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-11 04:27:27 +0000

    components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer-container">
+            <div>Copyright &copy; 2024</div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Grocery List</h1>
+        </header>
+    );
+}
+
+export default Header;
