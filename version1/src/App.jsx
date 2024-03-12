b220be303da53107ed03f59b348ecf29985bb9b7
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-12 21:53:56 +0000

    included new components

--- version1/src/App.jsx
+++ version1/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -7,33 +7,32 @@ import SearchItem from "./components/SearchItem";
 
 function App() {
     const [items, setItems] = useState(
-        JSON.parse(localStorage.getItem("shoppingList"))
+        JSON.parse(localStorage.getItem("shoppingList")) || []
     );
     const [search, setSearch] = useState("");
     const [newItem, setNewItem] = useState("");
 
-    const setAndSaveItems = (newItems) => {
-        setItems(newItems);
-        localStorage.setItem("shoppingList", JSON.stringify(newItems));
-    };
+    useEffect(() => {
+        localStorage.setItem("shoppingList", JSON.stringify(items));
+    }, [items]);
 
     const addItem = (item) => {
         const id = items.length + 1;
         const newItem = { id, item, checked: false };
         const listItems = [...items, newItem];
-        setAndSaveItems(listItems);
+        setItems(listItems);
     };
 
     const handleChekBox = (index) => {
         const listItems = items.map((item, i) =>
             i === index ? { ...item, checked: !item.checked } : item
         );
-        setAndSaveItems(listItems);
+        setItems(listItems);
     };
 
     const handleDelete = (index) => {
         const listItems = items.filter((_, i) => i !== index);
-        setAndSaveItems(listItems);
+        setItems(listItems);
     };
 
     const handleSubmit = (e) => {
