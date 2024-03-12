83353b742424b968e7fb6ae5ad2da1ff8668564b
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-12 21:56:00 +0000

    app js

--- color-mapper-project/src/App.jsx
+++ color-mapper-project/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Display from "./components/Display";
+import InputColor from "./components/InputColor";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [color, setColor] = useState("");
+    const [hexValue, setHexValue] = useState("");
+    const [isDarkText, setIsDarkText] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Display
+                color={color}
+                hexValue={hexValue}
+                isDarkText={isDarkText}
+            />
+            <InputColor
+                color={color}
+                setColor={setColor}
+                setHexValue={setHexValue}
+                isDarkText={isDarkText}
+                setIsDarkText={setIsDarkText}
+            />
+        </>
+    );
 }
 
-export default App
+export default App;
