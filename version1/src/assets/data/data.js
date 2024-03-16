0c158958dd0977827187de13c8eda30e99f318a5
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-16 23:43:24 +0000

    data.js

---
+++ version1/src/assets/data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        datetime: "10/12/2051",
+        title: "A new year update",
+        body: "Graph development entirely softly differ also east appropriate ready amount produce too cloth ball cut merely simple path human mud mile laid least perfectly",
+    },
+    {
+        id: 2,
+        datetime: "4/26/2123",
+        title: "War zone",
+        body: "Torn soft shaking function three hide fish spell paragraph married happy muscle remove layers society under lose adult most good little ship up rich",
+    },
+    {
+        id: 3,
+        datetime: "5/11/2115",
+        title: "The afficacy of the united Nation",
+        body: "Dust remove offer total treated fallen alphabet sick say structure crack car high phrase farm bark mighty are special arrangement laugh include blow accept",
+    },
+    {
+        id: 4,
+        datetime: "1/17/2024",
+        title: "Trending news in the new tech",
+        body: "Send effort foot establish nothing visitor does engineer everyone upward raw say successful arrive shelf constantly disease process rabbit right sick weight pleasant wish",
+    },
+]
