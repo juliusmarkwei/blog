b2b6fcbc9dfb2e81832eec0636907f4d3a8698d5
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-16 23:40:08 +0000

    missing component

--- version1/src/components/Missing.jsx
+++ version1/src/components/Missing.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Missing = () => {
-    return <div>Missing</div>;
+    return (
+        <div>
+            <p>Page not found</p>
+            <p>Well, that's dissapointing!</p>
+            <Link to="/">Visit our Homepage</Link>
+        </div>
+    );
 };
 
 export default Missing;
