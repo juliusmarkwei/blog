ec174caced00b9e7c4eca901d8dc7208e40c8ab1
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-16 23:41:03 +0000

    newpost page

--- version1/src/components/NewPost.jsx
+++ version1/src/components/NewPost.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-const NewPost = () => {
-    return <div>NewPost</div>;
+const NewPost = ({
+    handleSubmit,
+    postTitle,
+    setPostTitle,
+    postBody,
+    setPostBody,
+}) => {
+    return (
+        <main className="NewPost">
+            <form className="new-post-form" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title: </label><br />
+                <input
+                    id="postTitle"
+                    type="text"
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                /><br />
+                <label htmlFor="postBody">Post: </label><br />
+                <textarea
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                    cols="70"
+                    rows="8"
+                ></textarea><br />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+    );
 };
 
 export default NewPost;
