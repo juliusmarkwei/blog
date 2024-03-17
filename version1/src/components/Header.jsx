2ba1b4fbf2e0337200ce428745c2232b513653ce
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-03-17 03:55:10 +0000

    header

--- version1/src/components/Header.jsx
+++ version1/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
 
-const Header = ({ title }) => {
+const Header = ({ title, width }) => {
     return (
         <header className="blog-title">
             <h2>{title}</h2>
+            {width < 768 ? <FaMobileAlt />
+                : width < 992 ? <FaTabletAlt />
+                    : <FaLaptop />
+                    }
         </header>
-    ) 
+    );
 };
 
 export default Header;
